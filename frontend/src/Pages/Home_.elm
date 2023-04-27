f334a99ec34e16af2e8011c6f63499b1bd632b57
Mika Naylor <mail@autophagy.io>
2023-04-27 22:56:12 +0200

    Tweak splash image, get work details for home events
    In a messy way, but it can be cleaned up later

--- frontend/src/Pages/Home_.elm
+++ frontend/src/Pages/Home_.elm
@@ -4,6 +4,7 @@ import Api
 import Api.Event exposing (Event, getEventsWithLimit)
 import Api.Project exposing (Project, getProjects)
 import Api.State as State
+import Api.Work exposing (Work, getWork)
 import Dict exposing (Dict)
 import Dict.Extra exposing (groupBy)
 import Html exposing (Html)
@@ -28,24 +29,30 @@ page =
 -- INIT
 
 
+type alias EventWithWork =
+    { event : Event
+    , work : Work
+    }
+
+
 type alias Model =
     { projectData : Api.Data (List Project)
-    , eventsData : Api.Data (List Event)
+    , eventsData : List EventWithWork
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { projectData = Api.Loading, eventsData = Api.Loading }
+    ( { projectData = Api.Loading, eventsData = [] }
     , Cmd.batch [ getProjects { onResponse = ApiRespondedProjects }, getEventsWithLimit 5 { onResponse = ApiRespondedEvents } ]
     )
 
 
-groupEvents : List Event -> Dict String (List Event)
+groupEvents : List EventWithWork -> Dict String (List EventWithWork)
 groupEvents events =
     let
         eventToString =
-            \e -> String.join "." [ String.fromInt (toYear utc e.created_at), toMonthStr (toMonth utc e.created_at), String.pad 2 '0' <| String.fromInt (toDay utc e.created_at) ]
+            \e -> String.join "." [ String.fromInt (toYear utc e.event.created_at), toMonthStr (toMonth utc e.event.created_at), String.pad 2 '0' <| String.fromInt (toDay utc e.event.created_at) ]
     in
     groupBy eventToString events
 
@@ -57,6 +64,7 @@ groupEvents events =
 type Msg
     = ApiRespondedProjects (Result Http.Error (List Project))
     | ApiRespondedEvents (Result Http.Error (List Event))
+    | ApiRespondedWork Event (Result Http.Error Work)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -73,14 +81,20 @@ update msg model =
             )
 
         ApiRespondedEvents (Ok eventList) ->
-            ( { model | eventsData = Api.Success eventList }
+            ( model
+            , Cmd.batch (List.map (\event -> getWork event.work.id { onResponse = ApiRespondedWork event }) eventList)
+            )
+
+        ApiRespondedEvents (Err _) ->
+            ( model, Cmd.none )
+
+        ApiRespondedWork event (Ok work) ->
+            ( { model | eventsData = EventWithWork event work :: model.eventsData }
             , Cmd.none
             )
 
-        ApiRespondedEvents (Err err) ->
-            ( { model | eventsData = Api.Failure err }
-            , Cmd.none
-            )
+        ApiRespondedWork _ (Err _) ->
+            ( model, Cmd.none )
 
 
 
@@ -169,21 +183,27 @@ toMonthStr month =
             "12"
 
 
-viewEvent : Event -> Html Msg
-viewEvent event =
+viewEvent : EventWithWork -> Html Msg
+viewEvent e =
     let
+        event =
+            e.event
+
+        work =
+            e.work
+
         eventStr =
             case event.previous_state of
                 Just state ->
-                    "Work " ++ String.fromInt event.work.id ++ " transitioned from " ++ stateStr state ++ " to " ++ stateStr event.current_state ++ "."
+                    " transitioned from " ++ stateStr state ++ " to " ++ stateStr event.current_state ++ "."
 
                 Nothing ->
-                    "Work " ++ String.fromInt event.work.id ++ " was " ++ stateStr event.current_state ++ "."
+                    " was " ++ stateStr event.current_state ++ "."
     in
-    Html.div [] [ Html.text eventStr ]
+    Html.div [] [ Html.a [ Html.Attributes.href ("/works/" ++ String.fromInt work.id) ] [ Html.text work.name ], Html.text eventStr ]
 
 
-viewEvents : Dict String (List Event) -> List (Html Msg)
+viewEvents : Dict String (List EventWithWork) -> List (Html Msg)
 viewEvents events =
     let
         viewEventGroup =
@@ -210,15 +230,7 @@ view model =
                     Html.div [] [ Html.text "Failed to load :(" ]
 
         eventsView =
-            case model.eventsData of
-                Api.Success events ->
-                    Html.div [] (viewEvents (groupEvents events))
-
-                Api.Loading ->
-                    Html.div [] [ Html.text "..." ]
-
-                Api.Failure _ ->
-                    Html.div [] [ Html.text "Failed to load :(" ]
+            Html.div [] (viewEvents (groupEvents model.eventsData))
     in
     { title = "Wyrhta Ceramics"
     , body =
