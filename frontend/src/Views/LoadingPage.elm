beb124b59ec649ebc33c4df4bf1e0f201d6d4399
Mika Naylor <mail@autophagy.io>
2023-05-28 21:45:43 +0200

    Add CSS transition when a loadable element is loaded

--- frontend/src/Views/LoadingPage.elm
+++ frontend/src/Views/LoadingPage.elm
@@ -14,10 +14,10 @@ viewLoadingPage : (a -> PageState) -> a -> List (Html msg) -> Html msg
 viewLoadingPage f a e =
     case f a of
         Loaded ->
-            Html.div [ class "loaded" ] e
+            Html.div [ class "loading-page loaded" ] e
 
         Loading ->
-            Html.div [ class "loading" ] []
+            Html.div [ class "loading-page loading" ] []
 
         Error ->
-            Html.div [ class "error" ] [ Html.text ":(" ]
+            Html.div [ class "loading-page error" ] [ Html.text ":(" ]
