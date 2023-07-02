9f933a62bb6cef5da83ea0c50e94c43b41afbcfe
Mika <mail@autophagy.io>
2023-07-02 19:04:30 +0200

    Make some things clear

--- frontend/src/Pages/About.elm
+++ frontend/src/Pages/About.elm
@@ -81,6 +81,8 @@ pieces.
 I do my work at [Ceramic Kingdom](https://www.ceramickingdomberlin.com/), in Neukölln.
 You can find me elsewhere on the [Fediverse](https://hordburh.autophagy.io/@mika) or
 on [Github](https://github.com/autophagy).
+
+¡No pasarán!
 """
 
 
