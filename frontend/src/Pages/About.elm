589d97bc5672f57d24ef9b5235522f421b2d514c
Mika Naylor <mail@autophagy.io>
2024-11-14 20:56:56 +0100

    I'm not 80 years old. 'The Bluesky'??

--- frontend/src/Pages/About.elm
+++ frontend/src/Pages/About.elm
@@ -78,7 +78,7 @@ exposed clay and simple glazing. These days I'm mostly working on practicing
 throwing consistently, but have started taking requests from friends for specific
 pieces.
 
-You can find me elsewhere on the [Bluesky](https://bsky.app/profile/autophagy.io) or
+You can find me elsewhere on [Bluesky](https://bsky.app/profile/autophagy.io) or
 on [Github](https://github.com/autophagy).
 
 ¡No pasarán!
